bb9501ba1c1acc4be61db1365b459b081d651ddd
candela-oberstaff <candela@oberstaff.com>
2026-06-26 11:14:32 -0300

    fix deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// Detect if we are building for production
-const isProd = process.env.NODE_ENV === 'production';
-
 // https://astro.build/config
-export default defineConfig({
-  redirects: isProd ? {
-    '/pr': '/',
-    '/pr/[...slug]': '/',
-  } : {},
-});
+export default defineConfig({});
+
