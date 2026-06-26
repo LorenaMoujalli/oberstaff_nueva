5df311c69a16147e6ddb44990e2899dbd3b79775
Lorena Moujalli <lorenamoujalli860@gmail.com>
2026-06-26 09:54:18 -0400

    correcciones produccion pr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+// Detect if we are building for production
+const isProd = process.env.NODE_ENV === 'production';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  redirects: isProd ? {
+    '/pr': '/',
+    '/pr/[...slug]': '/',
+  } : {},
+});
