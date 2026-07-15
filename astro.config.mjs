191e3c82e07f94d5bfb0dcc99b7e484016fd3d1c
Lorena Moujalli <lorenamoujalli860@gmail.com>
2026-07-15 17:27:19 -0400

    correcciones faltantes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,14 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  vite: {
+    server: {
+      proxy: {
+        '/api/guardar-lead': {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+        }
+      }
+    }
+  }
 });
